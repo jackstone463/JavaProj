ee9865cb32271296a854af9fffb5aed1f2c337b7
jackstone463 <Jackstone463@gmail.com>
2024-09-07 13:33:17 -0400

    Add files via upload

---
+++ module1.1/module1.1/src/mod1E1.java
@@ -0,0 +1,13 @@
+public class mod1E1 {
+    public static void main(String[] args) throws Exception {
+        //printing out welcome to java
+		System.out.println("welcome to java");
+
+		//printing out welcome to computer science
+		System.out.println("welcome to Computer Science");
+
+		//printing out programming is fun
+		System.out.println("Programming is fun");
+    }
+}
+
