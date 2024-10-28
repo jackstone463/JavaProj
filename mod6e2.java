3f3cca96aa49db57d3cdedc652b9587c40139476
jackstone463 <Jackstone463@gmail.com>
2024-10-28 15:02:50 -0400

    Add files via upload

---
+++ mod6e2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class mod6e2{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter an integer to sum the digits of: ");
+        long value = in.nextLong();
+        System.out.println("The sum of all digits is: " + sumDigits(value));
+    }
+
+    public static int sumDigits(long n){
+        int temp = (int)Math.abs(n);
+        int sum = 0;
+        
+        while(temp != 0){
+            sum += (temp % 10);
+            temp = temp / 10;
+        }
+
+        return sum;
+    }    
+}
